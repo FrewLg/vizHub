86deff7010d9d74aa6a9c4fd0aa64f8e7f9c6704
Firra <frew.legese@gmail.com>
2026-09-15 10:52:40 +0300

    Deploy remote

--- VizHub/settings.py
+++ VizHub/settings.py
@@ -45,8 +45,7 @@ SECRET_KEY = env("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
-
+ALLOWED_HOSTS = ['*'] 
 
 # Application definition
 
@@ -77,8 +76,9 @@ UNFOLD = {
 }
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
+    'whitenoise.middleware.WhiteNoiseMiddleware',   
     "django.contrib.sessions.middleware.SessionMiddleware",
-'django.middleware.locale.LocaleMiddleware',           # <--- Added here
+    'django.middleware.locale.LocaleMiddleware',          
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
     "django.contrib.auth.middleware.AuthenticationMiddleware",
@@ -205,6 +205,8 @@ LOCALE_PATHS = [
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = "static/"
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
