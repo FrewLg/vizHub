894bd942a472e18fff78d63d9bbb0a1bd85f18ad
Frew Legese <frew.legese@gmail.com>
2026-09-21 21:50:35 +0000

    Dev

--- VizHub/settings.py
+++ VizHub/settings.py
@@ -50,8 +50,8 @@ ALLOWED_HOSTS = ['*']
 # Application definition
 
 INSTALLED_APPS = [
-'unfold',
- "django.contrib.admin",
+    # 'unfold',
+    "django.contrib.admin",
     "django.contrib.auth",
     "django.contrib.contenttypes",
     "django.contrib.sessions",
