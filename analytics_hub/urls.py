3737d696f33e56d71984c3804b778f9d99de0ec0
Firra <frew.legese@gmail.com>
2026-09-24 16:31:18 +0300

    GeoJSON added

--- analytics_hub/urls.py
+++ analytics_hub/urls.py
@@ -1,19 +1,16 @@
 from django.urls import path
 from . import views
-from django.db.models import Sum, Avg, Count, Max
 from .views import ObservationBarChartView
 
 urlpatterns = [
-    path('', views.dashboard_view, name='dashboard'),
+    path('viz/bar/old', views.dashboard_view, name='observation-bar-chart'),
     path('upload/', views.upload_view, name='upload_data'),
     path('export-pdf/', views.download_pdf_report, name='export_pdf'), 
-        path('api/chatbot/', views.ai_chatbot_api, name='ai_chatbot_api'),
-path(
-        "viz/bar/",
+    path('api/chatbot/', views.ai_chatbot_api, name='ai_chatbot_api'),
+    path(
+        "",
         ObservationBarChartView.as_view(),
-        name="observation-bar-chart",
+        name="dashboard",
     ),
-    path("api/analysis/<slug:slug>/", views.analysis_data     ), 
+    path("api/analysis/<slug:slug>/", views.analysis_data ), 
 ]
-
- 
